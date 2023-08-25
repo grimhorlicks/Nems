c54b7bd02dc9af2b50100c009d34fccc9c288120
Nemisha <104445880+Nemisha10@users.noreply.github.com>
2023-08-25 15:38:36 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
 
 
 import sys
 
-
-# In[3]:
-
-
 import joblib
 
 sys.modules['sklearn.externals.joblib'] = joblib
-
-
-# In[4]:
 
 
 def predict(data):
